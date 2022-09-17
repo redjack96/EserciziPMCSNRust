2e9eb541448030b9660100db5e83f06e21b99453
Giacomo <giacomo.redjack@gmail.com>
2022-09-17 19:09:39 +0200

    Added ex 1.2.2 & 1.2.3 to ssq1

--- src/ssq1.rs
+++ src/ssq1.rs
@@ -1,6 +1,6 @@
 /**
- Servente M/M/1/FIFO Trace-driven
- codice ripreso da ssq1.c
+Servente M/M/1/FIFO Trace-driven
+codice ripreso da ssq1.c
  **/
 use std::fs;
 use std::path::Path;
@@ -14,20 +14,37 @@ struct Adder {
     delay: f64,
     wait: f64,
     interarrival: f64,
+    last_departure: f64, // Es. 1.2.2
 }
 
 impl Adder {
+    fn throughput(&self, n: i64) -> f64 {
+        n as f64 / self.last_departure
+    }
     fn avg_interarrival(&self, index: i64) -> f64 {
         self.interarrival / index as f64
     }
-    pub fn avg_service(&self, index: i64) -> f64 {
+    fn avg_service(&self, index: i64) -> f64 {
         self.service / index as f64
     }
-    pub fn avg_delay(&self, index: i64) -> f64 {
+    fn avg_delay(&self, index: i64) -> f64 {
         self.delay / index as f64
     }
-    pub fn avg_wait(&self, index: i64) -> f64 {
+    fn avg_wait(&self, index: i64) -> f64 {
         self.wait / index as f64
+    }
+    // Es. 1.2.2
+    fn avg_population(&self, n: i64) -> f64 {
+        self.avg_wait(n) * self.throughput(n)
+    }
+    fn avg_queue_population(&self, n: i64) -> f64 {
+        self.avg_delay(n) * self.throughput(n)
+    }
+    fn avg_service_population(&self, n: i64) -> f64 {
+        self.avg_service(n) * self.throughput(n)
+    }
+    fn traffic_intensity(&self, n: i64) -> f64 {
+        self.avg_service(n) / self.avg_interarrival(n)
     }
 }
 
@@ -42,24 +59,28 @@ fn parse_doubles(line: &str) -> Vec<f64> {
 }
 
 fn get_arrival(doubles: &Vec<f64>) -> Option<f64> {
-    doubles.first().cloned()
+    doubles.first().cloned().map(|d| d*1.2)
 }
 
 fn get_service(doubles: &Vec<f64>) -> Option<f64> {
-    doubles.last().cloned()
+    // doubles.last().cloned()
+    Some(7.124851)
 }
 
 // Esegui con: cargo run --bin ssq1
 fn main() {
     let mut index = 0i64;
     let mut arrival = START;
-    let mut delay ;
+    let mut delay;
     let mut service;
-    let mut wait ;
+    let mut wait;
     let mut departure = START;
 
     let mut adder = Adder::default();
 
+    // Es. 1.2.3
+    let mut max_delay = 0.0;
+    let mut job_delayed : i64 = 0;
 
     println!("Hello, world!");
     // TRACE DRIVEN_ prende i dati da un file
@@ -72,6 +93,10 @@ fn main() {
         // Non c'è bisogno di una coda
         if arrival < departure {
             delay = departure - arrival;
+            job_delayed += 1;
+            if delay > max_delay {
+                max_delay = delay;
+            }
         } else {
             delay = 0.0;
         }
@@ -79,7 +104,7 @@ fn main() {
         service = get_service(&vec).unwrap_or(0.0);
         wait = delay + service;
         departure = arrival + wait;
-
+        adder.last_departure = departure; // c_i
         adder.delay += delay;
         adder.wait += wait;
         adder.service += service;
@@ -88,8 +113,18 @@ fn main() {
     adder.interarrival = arrival - START;
 
     println!("Per {} jobs", index);
-    println!("\tTempo di interarrivo medio = {}", adder.avg_interarrival(index));
-    println!("\tTempo di servizio medio    = {}", adder.avg_service(index));
-    println!("\tTempo di attesa medio      = {}", adder.avg_delay(index));
-    println!("\tTempo di risposta medio    = {}", adder.avg_wait(index));
+    println!("\tTempo di interarrivo medio (r) = {}", adder.avg_interarrival(index));
+    println!("\tTempo di servizio medio    (s) = {}", adder.avg_service(index));
+    println!("\tTempo di attesa medio      (w) = {}", adder.avg_delay(index));
+    println!("\tTempo di risposta medio    (d) = {}", adder.avg_wait(index));
+    // Esercizio 1.2.2 pag 25. Aggiungi l, q, x
+    println!("\tPopolazione media del nodo    (l) = {}", adder.avg_population(index));
+    println!("\tPopolazione media della coda  (q) = {}", adder.avg_queue_population(index));
+    println!("\tPopolazione media in servizio (x) = {} (utilizzazione)", adder.avg_service_population(index));
+    println!("\tThroughput                    (X) = {}", adder.throughput(index));
+    println!("\tIntensità di traffico       (s/r) = {}", adder.traffic_intensity(index));
+    println!("\tIstante di completamento    (c_n) = {}", adder.last_departure);
+    // Esercizio 1.2.3. Max d, # job al tempo t, percentuale di job che attendono.
+    println!("\tMassima attesa in coda    (max d) = {}", max_delay);
+    println!("\tProporzione di job che attendono  = {} %", job_delayed as f64 / index as f64 * 100.0);
 }
